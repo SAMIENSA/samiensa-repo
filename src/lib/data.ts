5f27b1535a284c7ca2d93a3f7e692ffc3f64962d
Khaled Mohamed <khalerdkolkolkol@gmail.com>
2025-08-22 18:05:49 +0000

    اين ذهب الواتس اب

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -406,7 +406,7 @@ export const portfolioData: PortfolioData = {
 
 export const socialLinks = [
     { name: 'Email', href: 'https://mail.google.com/mail/?view=cm&to=khalerdkolkolkol@gmail.com', icon: Mail },
-    { name: 'Phone', href: 'https://wa.me/+201060241230', icon: MessageCircle },
+    { name: 'WhatsApp', href: 'https://wa.me/+201060241230', icon: MessageCircle },
     { name: 'Telegram', href: 'https://t.me/samiensa3', icon: Send },
     { name: 'LinkedIn', href: 'https://www.linkedin.com/in/khaled-mohamed-a895a6320/', icon: Linkedin },
     { name: 'Instagram', href: 'https://www.instagram.com/samiensa1/', icon: Instagram },
